87d0149f87ea6461415a12209d3b10b6383e8596
Michal Biernacki <userofficeit@gmail.com>
2020-11-18 20:32:17 +0100

    Change layout colors

--- logic/informationManager.js
+++ logic/informationManager.js
@@ -1,6 +1,7 @@
 const playLoadingAnimation = () => {
     let text = document.createElement(`h2`)
     text.setAttribute("id", "loading-label")
+    text.style.color = "#8A8A8A"
     text.innerHTML = "Loading"
     let dots = ""
     document.getElementById("grid-container").appendChild(text)
